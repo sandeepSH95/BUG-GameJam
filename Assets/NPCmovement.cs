27a223d03aa0340b0e42a5803941c8aed8dae40d
sandeepSH95 <sandeep.hoonjan@network.rca.ac.uk>
2021-11-15 12:33:36 +0000

    NPC roll movement scene added

---
+++ Assets/NPCmovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCmovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject colliderObject;
+    [SerializeField]
+    private int speed = 300;
+    private bool isMoving = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Roll(Vector3 direction)
+    {
+        isMoving = true;
+
+        float remainingAngle = 90;
+        Vector3 rotationCenter = transform.position + direction / 2 + Vector3.down / 2;
+        Vector3 rotationAxis = Vector3.Cross(Vector3.up, direction);
+
+        while (remainingAngle > 0)
+        {
+            float rotationAngle = Mathf.Min(Time.deltaTime * speed, remainingAngle);
+            transform.RotateAround(rotationCenter, rotationAxis, rotationAngle);
+            remainingAngle -= rotationAngle;
+            yield return null;
+        }
+
+        isMoving = false;
+    }
+}
